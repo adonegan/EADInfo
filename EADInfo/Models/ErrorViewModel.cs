ebc1469eb31078a5d34a745c5cbd92352c3c26b8
adonegan <donegan.aileen@gmail.com>
2024-03-20 19:16:01 +0000

    initial commit

---
+++ EADInfo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EADInfo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
